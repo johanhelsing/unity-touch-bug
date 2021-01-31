b390c06782060ef5923a2b1e8ff62f68e922d804
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-01-31 12:33:28 +0100

    Add test setup that works with the old input system

---
+++ Assets/TouchColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchColor : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private MeshRenderer renderer;
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        renderer.material.color = Color.red;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        renderer.material.color = Color.blue;
+    }
+}
